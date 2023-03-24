ff0879133b4e6e4a2c2ee55432658d071fca9292
Nick Fox-Gieg <nick@fox-gieg.com>
2023-03-24 14:18:04 -0400

    Update abc_walker.py

--- abc_walker.py
+++ abc_walker.py
@@ -1,12 +1,28 @@
 import os
 from pyunpack import Archive
+import subprocess
+import sys
+
+argv = sys.argv
+argv = argv[argv.index("--") + 1:] # get all args after "--"
+
+inputPath = argv[0]
+
+def runCmd(cmd):
+    returns = ""
+    try:
+        returns = subprocess.check_output(cmd, text=True)
+    except subprocess.CalledProcessError as e:
+        returns = f"Command failed with return code {e.returncode}"
+    print(returns)
+    return returns   
 
 # traverse root directory, and list directories as dirs and files as files
-for root, dirs, files in os.walk("."):
+for root, dirs, files in os.walk(inputPath):
     path = root.split(os.sep)
     print((len(path) - 1) * '---', os.path.basename(root))
     for file in files:
-        os.system("mv *.obj ..")
-        #print(len(path) * '---', file)
+        runCmd(["mv", "*.obj", ".."])
+        print(len(path) * '---', file)
 
 # Archive('data.7z').extractall("<output path>")
